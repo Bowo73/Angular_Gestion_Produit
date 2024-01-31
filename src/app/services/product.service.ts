ddf9745bd7f803cf20b34b945ef878f5731e18a1
Bowo73 <146717402+Bowo73@users.noreply.github.com>
2024-01-31 18:28:32 +0100

    fix page d'accueil et ajout de produits en dur

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,7 +7,11 @@ import { Observable, of } from 'rxjs';
 })
 export class ProductService {
   private products = [
-    { id: 1, nom: 'Produit 1', texture: 'Texture 1', grammage: 500, couleur: 'Rouge' },
+    { id: 1, nom: 'Produit 1', texture: 'Texture 1', grammage: 100, couleur: 'Rouge' },
+    { id: 2, nom: 'Produit 2', texture: 'Texture 2', grammage: 200, couleur: 'Bleu' },
+    { id: 3, nom: 'Produit 3', texture: 'Texture 3', grammage: 300, couleur: 'Vert' },
+    { id: 4, nom: 'Produit 4', texture: 'Texture 4', grammage: 400, couleur: 'Jaune' },
+    { id: 5, nom: 'Produit 5', texture: 'Texture 5', grammage: 500, couleur: 'Noir' },
     // Ajoutez d'autres produits ici
   ];
 
